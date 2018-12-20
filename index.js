2d42afc62245c2c8ea9df87354a330d95aa469a3
Mahbubur Rahman <mail@mahbub.me>
2018-12-20 16:45:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector); 
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested .target");  
+}
+
+function increaseRankBy(n) {
+  var rankedList = document.querySelectorAll(".ranked-list li"); 
+  
+  for( let i = 0; i < rankedList.length; i++ ) {
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + parseInt(n);
+  }
+}
+
+function deepestChild() {
+  var deepestChild;
+  var nodes = document.getElementById("grand-node").querySelectorAll("div");
+  
+  for( let i = 0; i < nodes.length-1; i++ ) {
+    deepestChild = nodes[i].querySelector("div");
+  }
+  
+  return deepestChild;
+}
